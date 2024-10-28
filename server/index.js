2ed7886fec6fe8f9b3a3263950af192b5c9cb97a
Thushan Ekanayaka <sandaluthushan20@gmail.com>
2024-10-27 20:26:04 -0700

    done

--- server/index.js
+++ server/index.js
@@ -1,3 +1,26 @@
 import express  from "express";
+import mongoose  from "mongoose";
 const app =express();
- 
+
+// import dotenv from "dotenv";
+
+// dotenv.config();
+
+app.get('/', async (req, res) => {
+    res.send(" hello serever is ready to paly");
+})
+
+mongoose.connect('mongodb+srv://user1:Thush12213@cluster0.9qwykfs.mongodb.net/BookStrore?retryWrites=true&w=majority&appName=Cluster0')
+.then(()=>{
+    console.log("connected to the database sandalu 🚀🚀🚀🚀");
+
+    app.listen(3333,()=>{
+        console.log("server is running on port 3333");
+    
+    });
+})
+.catch((err)=>{
+    console.log("connection is faild",err);
+});
+
+console.log("hello there")
